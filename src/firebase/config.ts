9499f31e9dbc357d4f675a91f6a887ee3d18b90a
sabin-saji <theastralregulator@gmail.com>
2026-05-31 12:26:42 +0530

    Update Firebase configuration credentials to quub-vault-v1

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,11 @@
 // This is the public configuration for the Firebase project.
 // It is safe to use in the client-side code.
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDu6gwkxPLTlydZrRwXA7_Urgw_YOxP8hE",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "project-quub.firebaseapp.com",
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "https://project-quub-default-rtdb.firebaseio.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "project-quub",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "project-quub.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "940096047431",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:940096047431:web:adafc0c7938ddf94696462",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-Y2FVYY7TWJ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDE9heX5r1r9JwtZO8SHuianUaILcEMjSc",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "quub-vault-v1.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "quub-vault-v1",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "quub-vault-v1.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "885184734375",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:885184734375:web:271f21e6bc7eab93d92782",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-2N7ES4WWEJ"
 };
